b066bb4f614c35cdc4be2bb9ea0e75390742499e
Tanveer Ahmad Khan <tnvr000@gmail.com>
2024-07-14 11:10:43 +0530

    added new equipment page

--- app/controllers/equipments_controller.rb
+++ app/controllers/equipments_controller.rb
@@ -3,14 +3,32 @@ class EquipmentsController < ApplicationController
     @equipments = Equipment.all
   end
 
+  def new
+    @equipment = Equipment.new
+  end
+
+  def create
+    @equipment = Equipment.new(equipment_params)
+
+    if equipment.save
+      redirect_to(equipment_path(equipment))
+    else
+      render :new
+    end
+  end
+
   def show
     @equipment = set_equipment
   end
 
   private
 
+  def equipment_params
+    params.require(:equipment).permit(EQUIPMENT_PARAMS)
+  end
+
   def equipment
-    @equipment || set_equipment
+    @equipment ||= set_equipment
   end
 
   def set_equipment
--- app/helpers/equipments_helper.rb
+++ app/helpers/equipments_helper.rb
@@ -5,4 +5,31 @@ module EquipmentsHelper
 
     str + '*' * equipment.equipment_class_level
   end
+
+  def equipment_type_form_options
+    Equipment
+      .equipment_types
+      .map { |equipment_type, _index| [equipment_type.to_s.titlecase, equipment_type.to_s] }
+  end
+
+  def equipment_style_form_options
+    Equipment
+      .equipment_styles
+      .map { |equipment_style, _index| [equipment_style.to_s.titlecase, equipment_style.to_s] }
+  end
+
+  def equipment_class_form_options
+    Equipment
+      .equipment_classes
+      .map { |equipment_class, _index| [equipment_class.to_s.titlecase, equipment_class.to_s] }
+  end
+
+  def equipmetn_class_level_form_options
+    [
+      ['Select Class Level', 0],
+      ['*', 1],
+      ['**', 2],
+      ['***', 3]
+    ]
+  end
 end
