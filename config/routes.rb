09be89f3b3d2edfabdd1103b1d2fd80b9e5c255b
Tanveer Ahmad Khan <tnvr000@gmail.com>
2024-08-03 22:29:29 +0530

    added home, header and footer

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root 'pages#index'
+
   resources :heroes, only: %i[index show edit update]
   resources :equipments do
     member do
