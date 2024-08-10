84f1feac9342bdecf9354cbb92da77712a3b02f5
Tanveer Ahmad Khan <tnvr000@gmail.com>
2024-08-10 20:23:53 +0530

    refactored hero filter options

--- app/helpers/heroes_helper.rb
+++ app/helpers/heroes_helper.rb
@@ -33,31 +33,31 @@ module HeroesHelper
   def hero_type_filter_options
     Hero
       .hero_types
-      .map { |hero_type, value| [hero_type.to_s.titlecase, value] }
-      .unshift(['All Types', 0])
+      .map { |hero_type, value| [hero_type.to_s.titlecase, value, @hero_type == value] }
+      .unshift(['All Types', 0, @hero_type.to_i.zero?])
   end
 
   def hero_role_filter_options
     Hero
       .hero_roles
-      .map { |hero_class, value| [hero_class.to_s.titlecase, value] }
-      .unshift(['All Roles', 0])
+      .map { |hero_role, value| [hero_role.to_s.titlecase, value, @hero_role == value] }
+      .unshift(['All Roles', 0, @hero_role.to_i.zero?])
   end
 
   def hero_style_filter_options
     Hero
       .hero_styles
-      .map { |hero_style, value| [hero_style.to_s.titlecase, value] }
-      .unshift(['All Styles', 0])
+      .map { |hero_style, value| [hero_style.to_s.titlecase, value, @hero_style == value] }
+      .unshift(['All Styles', 0, @hero_style.to_i.zero?])
   end
 
   def hero_order_filter_options
     [
-      ['Combat Power', 'combat_power'],
-      ['Hit Point', 'hit_point'],
-      ['Defence', 'defence'],
-      ['Attack', 'attack'],
-      ['Speed', 'speed']
+      ['Combat Power', 'combat_power', @order_by == 'combat_power'],
+      ['Hit Point', 'hit_point', @order_by == 'hit_point'],
+      ['Defence', 'defence', @order_by == 'defence'],
+      ['Attack', 'attack', @order_by == 'attack'],
+      ['Speed', 'speed', @order_by == 'speed']
     ]
   end
 end
