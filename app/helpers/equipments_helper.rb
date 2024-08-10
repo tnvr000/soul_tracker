33bdf6885ce13d4bc6f9772d8156552d776d3f50
Tanveer Ahmad Khan <tnvr000@gmail.com>
2024-08-10 20:57:36 +0530

    refactored equipment filter options

--- app/helpers/equipments_helper.rb
+++ app/helpers/equipments_helper.rb
@@ -44,31 +44,31 @@ module EquipmentsHelper
   def equipment_style_filter_options
     Equipment
       .equipment_styles
-      .map { |equipment_style, value| [equipment_style.to_s.titlecase, value] }
-      .unshift(['All Styles', 0])
+      .map { |equipment_style, value| [equipment_style.to_s.titlecase, value, @equipment_style == value] }
+      .unshift(['All Styles', 0, @equipment_style.zero?])
   end
 
   def equipment_type_filter_options
     Equipment
       .equipment_types
-      .map { |equipment_type, value| [equipment_type.to_s.titlecase, value] }
-      .unshift(['All Types', 0])
+      .map { |equipment_type, value| [equipment_type.to_s.titlecase, value, @equipment_type == value] }
+      .unshift(['All Types', 0, @equipment_type.zero?])
   end
 
   def equipment_class_filter_options
     Equipment
       .equipment_classes
-      .map { |equipment_class, value| [equipment_class.to_s.titlecase, value] }
-      .unshift(['All Classes', 0])
+      .map { |equipment_class, value| [equipment_class.to_s.titlecase, value, @equipment_class == value] }
+      .unshift(['All Classes', 0, @equipment_class.zero?])
   end
 
   def equipment_class_level_filter_options
     [
-      ['All Class Level', -1],
-      ['-', 0],
-      ['*', 1],
-      ['**', 2],
-      ['***', 3]
+      ['All Class Level', -1, @equipment_class_level == -1],
+      ['-', 0, @equipment_class_level.zero?],
+      ['*', 1, @equipment_class_level == 1],
+      ['**', 2, @equipment_class_level == 2],
+      ['***', 3, @equipment_class_level == 3]
     ]
   end
 end
