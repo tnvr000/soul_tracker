1a5de6b63c8eb92fd13df468475e149e748610e8
Tanveer Ahmad Khan <tnvr000@gmail.com>
2025-02-15 16:43:02 +0000

    added import model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_01_174544) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_15_161222) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -75,6 +75,11 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_01_174544) do
     t.integer "count_offset", default: 0
     t.index ["unique_key"], name: "index_heroes_on_unique_key", unique: true
     t.index ["user_id"], name: "index_heroes_on_user_id"
+  end
+
+  create_table "imports", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "sessions", force: :cascade do |t|
